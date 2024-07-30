e2703feb1448c4425eda2f7ad7822ab6b57f3d26
sachithrakalani <kalanisathyangi@gmail.com>
2024-07-30 10:24:27 +0530

    Create Search input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Search from "./components/Search";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
 }
 
 export default App;
