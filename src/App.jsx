6866eb285c9fc345d301117dc8cd05255a3126e3
sachithrakalani <kalanisathyangi@gmail.com>
2024-07-31 10:01:56 +0530

    Creating state to save Food Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Search from "./components/Search";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
-      <Search />
+      <Search foodData={foodData} setFoodData={setFoodData} />
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "ee5bdc0c495a45eb9b9093bcde4393d1";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("Pizza");
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
       console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
