c860e381116fcd821abcfe83245ff3deab13bb48
sachithrakalani <kalanisathyangi@gmail.com>
2024-07-31 12:03:39 +0530

    Created Navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
+import Nav from "./components/Nav";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <FoodList foodData={foodData} />
     </div>
