d6044382bf3075e2e08b13f59b9b973e9db70d80
sachithrakalani <kalanisathyangi@gmail.com>
2024-08-01 09:37:57 +0530

    Complete Search bar styling

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -4,7 +4,7 @@ export default function FoodList({ foodData }) {
   return (
     <div>
       {foodData.map((food) => (
-        <FoodItem food={food} />
+        <FoodItem key={food.id} food={food} />
       ))}
     </div>
   );
